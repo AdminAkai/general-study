0283cbf2d7e1ec1a4100d9b3b0b126164fd55a8d
AdminAkai <akatoirotech@gmail.com>
2023-09-09 22:35:16 -0400

    removed font import and css styling as there are no custom fonts in

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import Landing from './containers/Landing/Landing'
+
 import './App.css'
-import './assets/fonts/stylesheet.css'
 
 function App() {
   return (
-    <>
-      <h1>TEST APPLICATION</h1>
-    </>
+    <Landing />
   )
 }
 
