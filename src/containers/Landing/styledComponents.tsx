3aa456ef9605c3dd1cffd2e581e69f5e2957840a
AdminAkai <akatoirotech@gmail.com>
2023-09-14 06:49:58 -0400

    removed all effects for title

--- src/containers/Landing/styledComponents.tsx
+++ src/containers/Landing/styledComponents.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const LandingPageContainer = styled.div`
   width: 100%;
@@ -33,62 +33,10 @@ export const LandingPageLogo = styled.div`
   }
 `
 
-const glitchCopy = css`
-  content: attr(data-text);
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`
-
-const glitchSteps = () => {
-  const arrayOfSteps = []
-  for (let i = 0; i++; i <= 20) {
-    arrayOfSteps.push(`
-      ${i * (1 / 20)} {
-        clip: rect(random(100) + px, 9999px, random(100) + px, 0);
-        transform: skew((random(100) / 100) + deg);
-      }
-    `)
-  }
-  return css`
-    ${arrayOfSteps.join('')}
-  `
-}
-
 export const LandingPageTitle = styled.h1`
   font-size: 8rem;
   color: ${({ theme }) => theme.Adminis};
   text-align: left;
-
-  position: relative;
-
-  @keyframes glitch-anim {
-    ${glitchSteps()}
-  }
-
-  @keyframes glitch-anim2 {
-    ${glitchSteps()}
-  }
-
-  &:before {
-    ${glitchCopy}
-    left: 2px;
-    text-shadow: -2px 0 #ff00c1;
-    clip: rect(44px, 450px, 56px, 0);
-    animation: glitch-anim 5s infinite linear alternate-reverse;
-  }
-
-  &:after {
-    ${glitchCopy}
-    left: -2px;
-    text-shadow:
-      -2px 0 #ff00c1,
-      2px 2px #ff00c1;
-    clip: rect(44px, 450px, 56px, 0);
-    animation: glitch-anim2 5s infinite linear alternate-reverse;
-  }
 
   @media screen and (max-width: ${({ theme }) => theme.mobileMediaQuery}) {
     font-size: 4rem;
