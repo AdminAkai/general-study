ef5f96347402c2749b7b4cc7555561d80d07d224
AdminAkai <akatoirotech@gmail.com>
2023-11-28 03:43:51 -0500

    tsconfig change

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -5,8 +5,7 @@ import GridPanel from 'src/shared/components/GridPanel'
 import Adminis from 'src/shared/components/Adminis'
 import PanelDescription from 'src/shared/components/PanelDescription'
 
-import JoshTrinidadPDF from '../../assets/JoshTrinidad.pdf'
-// import JoshTrinidadPDF from 'src/assets/JoshTrinidad.pdf'
+import JoshTrinidadPDF from 'src/assets/JoshTrinidad.pdf'
 
 import { GridItems } from './lib'
 import {
