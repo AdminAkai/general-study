5c089bc33e0d34807a89fd5015bc58eb82852243
AdminAkai <akatoirotech@gmail.com>
2023-11-28 17:27:12 -0500

    added aws-sdk

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import {
   InMemoryCache,
   ApolloProvider,
   gql,
+  createHttpLink,
 } from '@apollo/client'
+import { SSM } from 'aws-sdk'
+import { setContext } from '@apollo/client/link/context'
 
 import store, { persistor } from 'src/shared/redux/store.ts'
 
@@ -17,8 +20,30 @@ import './assets/fonts/stylesheet.css'
 
 import './main.css'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+
+  const { Parameter } = await new SSM()
+    .getParameter({
+      Name: 'GITHUB_TOKEN',
+      WithDecryption: true,
+    })
+    .promise()
+
+  return {
+    headers: {
+      ...headers,
+      authorization: Parameter ? `Bearer ${Parameter}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
